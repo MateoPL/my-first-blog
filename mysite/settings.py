720ffc6de5d33ec4c8f4b25ab375bbece6efb9ee
admin <matek1807@gmail.com>
2016-12-01 09:02:00 +0100

    dodano edycje hosta

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bj__$8as20e56n!t2yyg8+!x3#0ted#-%p*ce+e$&b-f%qf8ct'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -102,3 +102,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
